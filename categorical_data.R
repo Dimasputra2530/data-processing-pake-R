ff08db8d18b578e0b01cb16d8bb1b63137172035
Dimas Putra <112814587+Dimasputra2530@users.noreply.github.com>
2025-01-25 01:10:39 +0700

    Add files via upload

---
+++ categorical_data.R
@@ -0,0 +1,21 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age), #untuk menunjuk table mana yg mau di ambil pake $
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age) #ave untuk menghitung rata2
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country, #factor untk mengkonfersi data kategorikal menjadi angka
+                         levels = c('France', 'Spain', 'Germany'), #ketik f1 untuk liat info factor
+                         labels = c(1, 2, 3)) #untuk menyimpan kategori unik dari suatu variabel bertipe faktor
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
